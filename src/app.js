1c744d73bcf96af40e6ffc0e21f357ed121425c2
vitor <santos_vitao@hotmail.com>
2019-11-09 22:36:36 -0300

    profile create + update + authenticate

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
-
 import routes from './routes';
+
+import './database';
 
 class App {
   constructor() {
     this.server = express();
-    
     this.middlewares();
     this.routes();
   }
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'a66fb01a157500fce7aa6cf115be6634',
+  expiresIn: '7d',
+};
